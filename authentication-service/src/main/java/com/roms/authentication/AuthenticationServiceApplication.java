b709456a451345e41041b25f8f24ae7bd2ce8c27
Sahajk <sahajkhatri19@gmail.com>
2022-05-19 17:40:16 +0530

    changes

--- authentication-service/src/main/java/com/roms/authentication/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/roms/authentication/AuthenticationServiceApplication.java
@@ -29,9 +29,9 @@ public class AuthenticationServiceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		userRepository.save(new User("batman", passwordEncoder.encode("bruce"), "ROLE_USER"));
-		userRepository.save(new User("superman", passwordEncoder.encode("clark"), "ROLE_USER"));
-		userRepository.save(new User("ironman", passwordEncoder.encode("stark"), "ROLE_USER"));
-		userRepository.save(new User("spiderman", passwordEncoder.encode("peter"), "ROLE_USER"));
+		userRepository.save(new User("sahaj", passwordEncoder.encode("khatri"), "ROLE_USER"));
+		userRepository.save(new User("deepansh", passwordEncoder.encode("garg"), "ROLE_USER"));
+		userRepository.save(new User("sakshi", passwordEncoder.encode("bhat"), "ROLE_USER"));
+		userRepository.save(new User("hulk", passwordEncoder.encode("beast"), "ROLE_USER"));
 	}
 }
